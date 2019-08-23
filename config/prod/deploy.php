52ad834d5fb9ddcaa89af081a37c91945e006af2
Nicolas Gnyra <nicolas@gnyra.com>
2019-08-23 06:47:20 -0400

    Add deployment configuration

---
+++ config/prod/deploy.php
@@ -0,0 +1,30 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            ->server('deploy@ns509500.gnyra.com')
+            ->deployDir('/var/www/nicoco007.com/downloads-deploy')
+            ->repositoryUrl('git@github.com:nicoco007/download-manager')
+            ->repositoryBranch('develop')
+            ->sharedFilesAndDirs(['var/log', 'public/.htaccess', 'config/packages/prod/doctrine.yaml'])
+            ->composerInstallFlags('--prefer-dist --no-interaction --no-dev')
+            ->remoteComposerBinaryPath('composer');
+    }
+
+    public function beforePublishing()
+    {
+        $this->log('Installing dependencies');
+        $this->runRemote('yarn install');
+
+        $this->log('Running Webpack Encore');
+        $this->runRemote('yarn run encore production');
+
+        $this->log('Applying migrations');
+        $this->runRemote('{{ console_bin }} doctrine:migrations:migrate --no-interaction');
+    }
+};
